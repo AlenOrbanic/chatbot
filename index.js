cb05fef64b4aef7250ceac1824365e8510978e01
AlenOrbanic <aorbanic@student.unipu.hr>
2021-11-20 00:41:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ client.on('ready', () => {
   console.log('Bot je online');
 })
 const { MessageEmbed } = require('discord.js');
-const PREFIX = '';
+const PREFIX = '!';
 
-// https://discord.com/developers/docs/topics/gateway#list-of-intents
+var ctrl = false;
+var num = NaN;
 
 client.on('message', message=>{
 
@@ -18,21 +19,32 @@ client.on('message', message=>{
       case 'ping':
               message.channel.send('pong')
           break;
-      case 'pomoc':
-            message.channel.send('Korisne komande : obrisi, macka, embed, random ')
+      case "pomoc":
+            const pomoc = new MessageEmbed()
+              .setColor("#0099ff")
+              .setTitle("Korisne komande:")
+              .addField("!ping", "pong")
+              .addField("!pomoc", "Prikaze ovaj embed prozor.")
+              .addField("!random", "Izabere nasumičan broj između neka 2 broja koja mi upišemo.")
+              .addField("!obrisi", "Obrise poruke u kanalu (treba specificirat koliki broj poruka brišemo)")
+              .addField("!macka", "Posalje nasumicno odabranu sliku macke")
+              .addField("!github", "Prikaže prozor s informacijama o githubu studenta.")
+              .addField("!guess", "Pogodite zamišljen broj")
+              .setColor('0x00d4ff');
+            message.channel.send({ embeds: [pomoc] });
           break;
       case 'obrisi':
             if(!args[1]) return message.reply('Molimo upisite broj poruka koji zelite obrisati.')
                 message.channel.bulkDelete(args[1]);
           break;
-      case 'embed':
-            const embed = new MessageEmbed()
+      case 'github':
+            const github = new MessageEmbed()
                 .setTitle('Ovo je embed prozor')
                 .addField('‎', '[Link na moj github](https://github.com/AlenOrbanic/)')
                 .setImage('https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU')
                 .setFooter('Source code chatbota je dostupan svima na korištenje.')
                 .setColor('0x00d4ff');
-            message.channel.send({ embeds: [embed] });
+            message.channel.send({ embeds: [github] });
           break;
       case 'macka':
             if (message.content.startsWith (PREFIX + "macka")) {
@@ -48,7 +60,24 @@ client.on('message', message=>{
             var result = Math.floor(Math.random() * (last + 1 - first)) + first;
             message.reply(String(result));
           break;
-    
+      case "guess":
+            if (!args[1]) {
+              message.reply("Picking a random number between 1 and 100");
+              num = Math.floor(Math.random() * 100 + 1);
+              guesses = 0;
+              ctrl = true;
+            } else if (args[1] == num && ctrl) {
+              guesses++;
+              message.reply("You got it! Only took " + guesses + " tries.");
+              ctrl = false;
+            } else if (args[1] < num && ctrl) {
+              message.reply(args[1] + " is too low");
+              guesses++;
+            } else if (args[1] > num && ctrl) {
+              message.reply(args[1] + " is too high");
+              guesses++;
+            }
+          break;
   }
 })
 
